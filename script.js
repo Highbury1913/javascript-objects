5a26205afc8c09db1e0643c9767d00e8477b4aad
Highbury1913 <wilensky.jason13@gmail.com>
2016-11-03 13:30:26 +0200

    hungry

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 var person = {
-	name:"J",
-	age:"W",
-	posessions: {
-		key:" came in with 1 set of keys, ",
-		cash:"$200, ",
-		pocketSquare:"1 pocket square",
-	},
-	dead: function() {
-		if (person.name === "Unknown" && person.age === "Unknown") {
-			console.log("John Doe " + person.posessions.key + person.posessions.cash + person.posessions.pocketSquare);
-		}
-		else if (person.name != "Unkown" && person.age != "Unknown" ){
-			console.log(person.name + person.age + person.posessions.key + person.posessions.cash + person.posessions.pocketSquare);
-		}
-	}
+  hungry: true,
 
+  feed: function () {
+    if (this.hungry) {
+      hungry = false;
+      alert('Im no longer hungry!');
+    }
+  }
 }
-person.dead()
+
+person.feed();
